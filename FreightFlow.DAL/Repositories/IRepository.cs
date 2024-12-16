14b0306950c58180ca86d6d2d7aca2f1caa44812
niazananev698 <niazananev698@gmail.com>
2024-12-16 21:38:39 +0500

    fix[IRepository.cs, Repository.cs]: add repository

---
+++ FreightFlow.DAL/Repositories/IRepository.cs
@@ -0,0 +1,73 @@
+using System.Linq.Expressions;
+
+namespace FreightFlow.DAL.Repositories;
+
+/// <summary>
+/// Общий интерфейс репозитория для управления сущностями типа <typeparamref name="T"/>.
+/// </summary>
+public interface IRepository<T> where T : class
+{
+    /// <summary>
+    /// Асинхронно получает коллекцию сущностей <typeparamref name="T"/> из базы данных с возможностью фильтрации, сортировки и включения связанных сущностей.
+    /// </summary>
+    /// <param name="includeProperties">Массив строк с названиями связанных сущностей, которые нужно включить</param>
+    /// <returns>Задача, возвращающая коллекцию сущностей <typeparamref name="T"/></returns>
+    Task<IEnumerable<T>?> GetAllAsync(params string[] includeProperties);
+
+    /// <summary>
+    /// Асинхронно получает сущность <typeparamref name="T"/> из базы данных по её идентификатору.
+    /// </summary>
+    /// <param name="id">Идентификатор сущности</param>
+    /// <param name="includeProperties">Массив строк с названиями связанных сущностей, которые нужно включить</param>
+    /// <returns>Задача, возвращающая сущность <typeparamref name="T"/> с указанным идентификатором</returns>
+    Task<T?> GetByIdAsync(int id, params string[] includeProperties);
+
+    /// <summary>
+    /// Асинхронно создаёт новую сущность <typeparamref name="T"/> в базе данных.
+    /// </summary>
+    /// <param name="entity">Создаваемая сущность</param>
+    /// <returns>Задача, представляющая создание сущности <typeparamref name="T"/></returns>
+    Task CreateAsync(T entity);
+
+    /// <summary>
+    /// Асинхронно удаляет сущность <typeparamref name="T"/> из базы данных по её идентификатору.
+    /// </summary>
+    /// <param name="id">Идентификатор удаляемой сущности</param>
+    /// <returns>Задача, представляющая удаление сущности <typeparamref name="T"/></returns>
+    Task DeleteAsync(int id);
+
+    /// <summary>
+    /// Асинхронно удаляет сущность <typeparamref name="T"/> из базы данных.
+    /// </summary>
+    /// <param name="entity">Удаляемая сущность</param>
+    /// <returns>Задача, представляющая удаление сущности <typeparamref name="T"/></returns>
+    Task Delete(T entity);
+
+    /// <summary>
+    /// Асинхронно обновляет сущность <typeparamref name="T"/> в базе данных.
+    /// </summary>
+    /// <param name="entity">Обновляемая сущность</param>
+    /// <returns>Задача, представляющая обновление сущности <typeparamref name="T"/></returns>
+    Task Update(T entity);
+
+    /// <summary>
+    /// Асинхронно возвращает первую сущность, удовлетворяющую указанному условию, или null, если сущность не найдена.
+    /// </summary>
+    /// <param name="predicate">Условие для фильтрации сущностей</param>
+    /// <returns>Задача, возвращающая первую сущность <typeparamref name="T"/> или null</returns>
+    Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
+
+    /// <summary>
+    /// Асинхронно возвращает единственную сущность, удовлетворяющую указанному условию, или null, если сущность не найдена.
+    /// </summary>
+    /// <param name="predicate">Условие для фильтрации сущностей</param>
+    /// <returns>Задача, возвращающая единственную сущность <typeparamref name="T"/> или null</returns>
+    Task<T?> SingleOrDefaultAsync(Expression<Func<T, bool>> predicate);
+
+    /// <summary>
+    /// Асинхронно проверяет, существует ли хотя бы одна сущность, удовлетворяющая указанному условию.
+    /// </summary>
+    /// <param name="predicate">Условие для фильтрации сущностей</param>
+    /// <returns>Задача, возвращающая true, если сущность найдена, иначе false</returns>
+    Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+}
---
+++ FreightFlow.DAL/Repositories/Repository.cs
@@ -0,0 +1,92 @@
+using System.Linq.Expressions;
+using FreightFlow.DAL.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreightFlow.DAL.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly ApplicationDbContext _dbContext;
+    private readonly DbSet<T> _dbSet;
+
+    public Repository(ApplicationDbContext context)
+    {
+        _dbContext = context;
+        _dbSet = _dbContext.Set<T>();
+    }
+
+    public async Task<bool> AnyAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.AnyAsync(predicate);
+    }
+
+    public async Task CreateAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task Delete(T entity)
+    {
+        _dbSet.Remove(entity);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        T? entityToDelete = await _dbSet.FindAsync(id);
+        if (entityToDelete != null)
+        {
+            _dbSet.Remove(entityToDelete);
+        }
+    }
+
+    public async Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.FirstOrDefaultAsync(predicate);
+    }
+
+    public async Task<IEnumerable<T>?> GetAllAsync(params string[] includeProperties)
+    {
+        IQueryable<T> query = _dbSet;
+
+        if (includeProperties != null)
+        {
+            query = includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+        }
+
+        return await query.ToListAsync();
+    }
+
+    public async Task<T?> GetByIdAsync(int id, params string[] includeProperties)
+    {
+        if (id <= 0)
+        {
+            throw new ArgumentException("ID must be greater than zero.", nameof(id));
+        }
+
+         IQueryable<T> query = _dbSet;
+
+        if (includeProperties != null && includeProperties.Length > 0)
+        {
+            foreach (var includeProperty in includeProperties)
+            {
+                query = query.Include(includeProperty);
+            }
+        }
+
+        return await query.FirstOrDefaultAsync(entity => EF.Property<int>(entity, "Id") == id);
+    }
+
+    public async Task<T?> SingleOrDefaultAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.SingleOrDefaultAsync(predicate);
+    }
+
+    public async Task Update(T entity)
+    {
+        _dbSet.Attach(entity);
+        _dbContext.Entry(entity).State = EntityState.Modified;
+        await _dbContext.SaveChangesAsync();
+    }
+}
