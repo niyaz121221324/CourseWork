c259fdbd31cdad0182569d5af5afc70e668b2bf9
niazananev698 <niazananev698@gmail.com>
2024-12-18 11:41:01 +0500

    fix[Program.cs]: edit DI

--- blazor-client/Program.cs
+++ blazor-client/Program.cs
@@ -6,10 +6,8 @@ using blazor_client.Models;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-// Correctly register the generic repository
+// Добавляем сервисы в DI
 builder.Services.AddScoped<IRepository<User>, Repository<User>>();
-
-// Register HttpClient as a singleton with the correct base address
 builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri("http://freightflow.api:8080/api") });
 
 builder.RootComponents.Add<App>("#app");
